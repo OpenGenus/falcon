87d94f8868da52f77be40ab67754057e2cb9828f
pratiksinghchauhan <pratiksingh773@gmail.com>
2018-12-30 21:09:03 +0530

    modified parameters in search

--- search.py
+++ search.py
@@ -2,13 +2,26 @@ import pickle
 import collections
 import json
 import sys
+import argparse
 
 trie = pickle.load( open( "dumps/trie.p", "rb" ) )  
 catMap = pickle.load( open( "dumps/catMap.p", "rb" ) )
 category = json.load( open( "dumps/category.json", "r" ) )
 
 
-searchTerm = " ".join(sys.argv[1:])
+parser = argparse.ArgumentParser()
+parser.add_argument("--search", help='The search term')
+parser.add_argument("--results", help='Required number of results')
+
+args = parser.parse_args()
+
+searchTerm = args.search
+display = args.results
+
+if searchTerm==None:
+    print("Enter a valid Search Term")
+    sys.exit()
+
 searchTerms = searchTerm.split()
 results = []
 for terms in searchTerms:
@@ -21,6 +34,15 @@ counts = collections.Counter(results)
 results = sorted(results, key=lambda x: -counts[x])
 searchResults = []
 [searchResults.append(x) for x in results if x not in searchResults]
+
+idx = 1
+if display:
+    for data in searchResults:
+        print category[data]['description']
+        idx = idx+1
+        if idx>int(display):
+            sys.exit()
+
 
 for data in searchResults:
     print category[data]['description']
