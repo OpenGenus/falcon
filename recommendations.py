f28bb25838150804ff73fc57016ca64a1691eb07
pratiksinghchauhan <pratiksingh773@gmail.com>
2019-01-21 03:52:50 +0530

    added final recommendation list

--- recommendations.py
+++ recommendations.py
@@ -65,13 +65,30 @@ def getChildWeightedList(childPath):
         p, s = q.pop(0)
   return childWeights
 
+
 allPaths = []
 childPath=[]
 getPath('artificial_intelligence',baseDict,'',allPaths,childPath)
 childPath = childPath[0]
 print(allPaths)
 print(getMaxDepth(allPaths))
-print(getParentWeightedList(allPaths))
+parentWeights = getParentWeightedList(allPaths)
 # print childPath
 childWeights = getChildWeightedList(childPath)
-print childWeights
+# print childWeights
+recDict = []
+maxChildWeight = len(childWeights) + 1
+
+for childs in childWeights:
+  for child in childs:
+    recDict.append({child:maxChildWeight})
+  maxChildWeight = maxChildWeight -1
+
+maxParentWeight = len(parentWeights)
+for parents in parentWeights:
+  for parent in parents:
+    recDict.append({parent:maxParentWeight})
+  maxParentWeight = maxParentWeight -1
+
+finalRecDict = sorted(recDict, key=lambda x: x.values(),reverse=True)
+print finalRecDict
