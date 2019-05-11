0977b9356f5e37b90c9c2cd243139e70b829312c
yedhink <yedhin1998@gmail.com>
2019-05-11 07:51:12 +0530

    update repo cloning with directory checks and embed within class for extensibility

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 from git import Repo
 import os
+from pathlib import Path
 
-gitUrl = "https://github.com/OpenGenus/cosmos.git"
-repoDir = os.path.dirname(os.path.abspath(__file__)) +"/cosmos"
+class Cosmos:
+    def clone_repo(self):
+        p = Path("./cosmos")
+        if p.exists():
+            return True
+        else:
+            gitUrl = "https://github.com/OpenGenus/cosmos.git"
+            repoDir = os.path.dirname(os.path.abspath(__file__)) +"/cosmos"
 
-try:
-    Repo.clone_from(gitUrl, repoDir)
-except Exception as e:
-    print(e)
+            try:
+                Repo.clone_from(gitUrl, repoDir)
+            except Exception as e:
+                return False
+                print(e)
+            return True
