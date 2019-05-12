4aa8b6b6c4f490f92fca4cb5a6bf64ce5d52ad77
yedhink <yedhin1998@gmail.com>
2019-05-12 07:19:10 +0530

    update script for directly cloning any directory by just executing the fiel

--- index.py
+++ index.py
@@ -1,20 +1,37 @@
 from git import Repo
 import os
 from pathlib import Path
+import argparse
 
 
 class Cosmos:
+    def __init__(self, repo_url="https://github.com/OpenGenus/cosmos.git"):
+        self.git_url = repo_url
+        self.directory = "/" + self.git_url.split("/")[-1].split(".")[0]
+        self.repo_dir = os.path.dirname(os.path.abspath(__file__)) + self.directory
+
     def clone_repo(self):
-        p = Path("./cosmos")
+        p = Path(self.directory)
         if p.exists():
             return True
         else:
-            gitUrl = "https://github.com/OpenGenus/cosmos.git"
-            repoDir = os.path.dirname(os.path.abspath(__file__)) + "/cosmos"
-
             try:
-                Repo.clone_from(gitUrl, repoDir)
+                Repo.clone_from(self.git_url, self.repo_dir)
             except Exception as e:
                 return False
                 print(e)
             return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Clone any git repo")
+    parser.add_argument(
+        "-c",
+        "--clone",
+        help="Repo to be cloned",
+        default="https://github.com/OpenGenus/cosmos.git",
+    )
+    results = parser.parse_args()
+    choice = results.clone
+    cloner = Cosmos(choice)
+    cloner.clone_repo()
