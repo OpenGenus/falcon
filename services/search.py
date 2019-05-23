e42e2f4f3a79f426c53dff831a2c5f1979b6bfff
yedhink <yedhin1998@gmail.com>
2019-05-23 23:29:46 +0530

    add functional code for getting args from openfalcon interface

--- services/search.py
+++ services/search.py
@@ -1,19 +1,25 @@
 import sys
 import argparse
-import getSearchResults
+from . import getSearchResults
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--search", help='The search term')
-parser.add_argument("--results", help='Required number of results')
 
-args = parser.parse_args()
+def main(*args):
+    if len(args) == 0:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--search", help="The search term")
+        parser.add_argument("--results", help="Required number of results")
+        args = parser.parse_args()
+        searchTerm = args.search
+        display = args.results
+    else:
+        searchTerm = args[0]
+        display = args[1]
+    if searchTerm == None:
+        print("Enter a valid Search Term")
+        sys.exit()
 
-searchTerm = args.search
-display = args.results
+    print(getSearchResults.getSearchResults(searchTerm, display))
 
-if searchTerm==None:
-    print("Enter a valid Search Term")
-    sys.exit()
 
-getSearchResults.getSearchResults(searchTerm,display)
-
+if __name__ == "__main__":
+    main()
