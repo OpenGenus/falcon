e0b4d7013d737a2342510949e7573e3652af2efe
yedhink <yedhin1998@gmail.com>
2019-05-31 18:56:18 +0530

    integrate recommendation module into the interface

--- services/openfalcon.py
+++ services/openfalcon.py
@@ -1,7 +1,7 @@
 """
                      ___     __
  ___  ___  ___ ___  / _/__ _/ /______  ___
-/ _ \/ _ \/ -_) _ \/ _/ _ `/ / __/ _ \/ _ \
+/ _ \/ _ \/ -_) _ \/ _/ _ `/ / __/ _ \/ _ |
 \___/ .__/\__/_//_/_/ \_,_/_/\__/\___/_//_/
    /_/
 
@@ -12,6 +12,7 @@ An interface to all the services in falcon
 import argparse as arg
 import sys
 from . import search
+from . import recommendations
 
 
 def init_search_module_args(parser):
@@ -23,16 +24,29 @@ def init_search_module_args(parser):
     )
 
 
+def init_recommendation_module_args(parser):
+    parser.add_argument("--recommend", help="The recommendation term")
+    parser.add_argument("--top", default=5, help="Number of results to show", type=int)
+    parser.add_argument(
+        "--type", default="all", help="Type of recommendation can be parent/child/all"
+    )
+
+
 def main():
     parser = arg.ArgumentParser("openfalcon")
     init_search_module_args(parser)
+    init_recommendation_module_args(parser)
     args = parser.parse_args()
-    # results arg can only be used when search arg is present
-    if args.results and (args.search is None):
-        parser.error("--results requires --search argument")
-        sys.exit()
+
     if len(sys.argv) == 1:
         print("Please refer to help section using openfalcon --help / openfalcon -h")
         sys.exit()
+
     if args.search:
+        # results arg can only be used when search arg is present
+        if args.results and (args.search is None):
+            parser.error("--results requires --search argument")
+            sys.exit()
         search.main(args.search, args.results)
+    if args.recommend:
+        recommendations.main(args.recommend, args.top, args.type)
