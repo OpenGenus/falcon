420c76f96f07377c373b99d46e56f32de37b6ac4
yedhink <yedhin1998@gmail.com>
2019-06-08 09:05:35 +0530

    remove default values for args + integrate all other modules including index

--- services/openfalcon.py
+++ services/openfalcon.py
@@ -15,23 +15,20 @@ from . import search
 from . import recommendations
 from . import code
 from . import stats
+from . import index
 
 
 def init_search_module_args(parser):
     parser.add_argument(
         "--search", help="Search for occurence of a term in Cosmos", type=str
     )
-    parser.add_argument(
-        "--results", default=5, help="Number of search results", type=int
-    )
+    parser.add_argument("--results", help="Number of search results", type=int)
 
 
 def init_recommendation_module_args(parser):
     parser.add_argument("--recommend", help="The recommendation term")
-    parser.add_argument("--top", default=5, help="Number of results to show", type=int)
-    parser.add_argument(
-        "--type", default="all", help="Type of recommendation can be parent/child/all"
-    )
+    parser.add_argument("--top", help="Number of results to show", type=int)
+    parser.add_argument("--type", help="Type of recommendation can be parent/child/all")
 
 
 def init_code_module_args(parser):
@@ -47,7 +44,12 @@ def init_stats_module_args(parser):
         "-f",
         "--format",
         help='Ouput can be "txt" for text file and "md" for markdown or "all" for all formats',
-        default="txt",
+    )
+
+
+def init_index_module_args(parser):
+    parser.add_argument(
+        "-c", "--clone", action="store_true", help="Clone the cosmos repo"
     )
 
 
@@ -57,14 +59,21 @@ def main():
     init_recommendation_module_args(parser)
     init_code_module_args(parser)
     init_stats_module_args(parser)
+    init_index_module_args(parser)
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
         print("Please refer to help section using openfalcon --help / openfalcon -h")
         sys.exit()
 
+    if args.clone:
+        c_repo = index.Cosmos()
+        if not c_repo.clone_repo():
+            print("Clone the cosmos repo manually")
+            exit(0)
     if args.format:
         stats.main(args.format)
+        exit(0)
     if args.search:
         # results arg can only be used when search arg is present
         if args.results and (args.search is None):
